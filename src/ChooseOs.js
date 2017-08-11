c2ddb4ce462948842beb315950edb5047e1fc98b
Per Wikman <Per.Wikman@pensionsmyndigheten.se>
2017-08-11 16:29:42 +0200

    add function for updating data

---
+++ src/ChooseOs.js
@@ -0,0 +1,12 @@
+chooseOs = function(os){
+
+  if(os == "Linux"){
+    alert(1);
+      $.getScript("data/survivaldata_Microsoft.js");
+  }else if(os == "Microsoft"){
+    alert(2)
+  }else{
+    alert(3)
+  }
+
+}
